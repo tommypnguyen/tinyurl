3c47591a8ce32a35e92bb2ddfb1b47e2245172b5
Tommy nguyen <tommypn1@uci.edu>
2018-12-27 12:11:07 -0800

    Psuedo code for short file

---
+++ short.py
@@ -0,0 +1,29 @@
+#Class short
+
+"""
+This class will take a large url and a short url representation of it
+A user can take this short url to take them to the long url
+Example:
+    input: https://www.google.com/search?qfqfqf
+    output: https://short.com/asdfa
+"""
+
+# function to convert large url to short url
+
+# function to convery short url to large url
+
+# random function to generate short url
+
+# function to check if url has been already created, if so return the short url already generated
+
+# we will need 5 character digits to make 916,137,832
+# a-z, 0-9, A-Z
+# rfc: https://www.ietf.org/rfc/rfc1738.txt
+# 26 + 10 + 26 = 62
+# 1 million urls in 6 months
+# 2 million urls in 12 months
+# 16 million urls in 8 years for an average life of a company
+# somename.com/aaaaa
+
+
+
